ccb424d167c328461b045940c0775f80744daab6
Andreas Hejndorf <andreas@hejndorf-foto.dk>
2020-12-05 12:13:36 +0100

    RUN Supervisor

--- application.py
+++ application.py
@@ -1,13 +1,7 @@
 from app import create_app
-from rq import Connection, Worker
-import multiprocessing
-
 
 application = create_app()
 
 if __name__ == '__main__':
-    with Connection(application.redis):
-        multiprocessing.Process(target=Worker(application.config['QUEUE']).work())
-
     application.debug = True
     application.run(host='0.0.0.0', threaded=True)
