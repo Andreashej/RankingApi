cf0766d9c265483e08cc94e037f8852db432ce99
Andreas Hejndorf <andreas@hejndorf-foto.dk>
2022-01-07 00:25:18 +0100

    Rename access_token to accessToken

--- app/v2/resources/Auth.py
+++ app/v2/resources/Auth.py
@@ -65,7 +65,7 @@ class TokenRefresh(Resource):
         current_user = get_jwt_identity()
 
         access_token = create_access_token(identity=current_user)
-        return { 'access_token': access_token }
+        return { 'accessToken': access_token }
 
 class ProfileResource(Resource):
     @jwt_required
